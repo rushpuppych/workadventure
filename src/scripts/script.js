38df1abe1c4a8f5b8a94d550fe623b0451852b74
rushpuppych <rushpuppych@users.noreply.github.com>
2022-01-27 22:59:58 +0000

    Deploying to gh-pages from master @ 9fef6dddaf9808ddab4bd22ec6227d4f74c89280 🚀

---
+++ src/scripts/script.js
@@ -0,0 +1,6 @@
+WA.chat.onChatMessage((message => {
+	if(message === "easter") {
+		WA.chat.sendChatMessage('WC', 'The Ghost of ICT');
+		WA.chat.sendChatMessage('SüdWest', 'The Ghost of ICT');
+	}
+}));
